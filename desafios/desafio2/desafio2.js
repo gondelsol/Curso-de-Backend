eb2b1cae3715a3194845777628eee956e9dbea93
gondelsol <gondelsol@gmail.com>
2022-08-03 18:40:04 -0300

    desafio2 terminado

--- desafios/desafio2/desafio2.js
+++ desafios/desafio2/desafio2.js
@@ -1,34 +1,42 @@
-const {    error } = require('console');
+const { error } = require('console');
+
 const fs = require('fs');
-const id = 8;
+//const id = 8;
 const indeseado = 3;
-const ruta = './bazar.json'
+const ruta = './bazar.txt'
+
 const productList = [];
+
 const product1 = {
     //id: 1,
     nombre: 'taza',
     precio: 3
 };
+
 const product2 = {
     //id: 2,
     nombre: 'jarra',
-    precio: 32
+    precio: 40
 };
+
 const product3 = {
     //id: 3,
     nombre: 'botella',
-    precio: 5
+    precio: 50
 };
+
 const product4 = {
     //id: 4,
     nombre: 'cuchillo',
-    precio: 5
+    precio: 65
 };
+
 const product5 = {
     //id: 5,
     nombre: 'tenedor',
-    precio: 5
+    precio: 895
 };
+
 const product6 = {
     //id: 6,
     nombre: 'taza',
@@ -37,109 +45,155 @@ const product6 = {
 
 // obj = [1, 2, 3, 4, 5]
 
-
-
 class Contenedor {
+
     constructor(ruta) {
-        this.ruta=ruta
-        console.log(ruta)
-
+        this.ruta = ruta
     }
-    async leer () {
 
-        try {
+    //El método ok funciona ok
 
+    async leer() {
+
+            try {
             const data1 = await fs.promises.readFile(ruta, 'utf-8');
-//            console.log(data1);
             const listaJSON = JSON.parse(data1);
-            console.log('La lista de productos es: ', listaJSON);
+            console.log(listaJSON);
             return listaJSON;
-
         } catch (error) {
+
             console.log('Error al leer el archivo json');
             console.log(error);
-            return error; 
+            return 'La lista de productos es: ' + error;
         }
 
-    }
-    async save(product) {
 
+    }
+    //El metodo save fuciona ok
+
+    async save(product) {
         const listaProductos = await this.leer();
         let newId;
-
         if (listaProductos.length == 0) {
             newId = 1;
         } else {
-            newId = listaProductos[listaProductos.length -1].id +1;
+            newId = listaProductos[listaProductos.length - 1].id + 1;
         }
-        console.log("El nuevo id es: ", newId)
-
-        const newProduct = { id: newId, ...product}
+        console.log('El nuevo id es: ', newId)
+        const newProduct = { id: newId, ...product }
         listaProductos.push(newProduct);
-
         await fs.promises.writeFile(this.ruta, JSON.stringify(listaProductos, null, 2))
-
         await this.leer();
-
     }
 
-    async actalizar (id, product) {
+    //El metodo actualizar funciona ok
+
+    async actalizar(id, product) {
+
+        console.log('Actualizar elementos', id, product)
+
         try {
             const listaProductos = await this.leer();
             const indexProduct = listaProductos.findIndex((o) => o.id == id);
-            if (indexProduct == -1 ) {
-                console.log('Objeto no encontrado')
+
+            if (indexProduct == -1) {
+                console.log('producto no encontrado')
                 return 'Busqueda erronea'
             } else {
-                listaProductos[indexProduct] = {id, ...product}; //Acá está el problema
-                listaProductos.push(listaProductos[indexProduct]); //Acá está el problema
-                await fs.promises.writeFile(this.ruta, JSON.stringify(listaProductos, null, 2)); //Acá está el problema
+                listaProductos[indexProduct] = { id, ...product };
+                await fs.promises.writeFile(this.ruta, JSON.stringify(listaProductos, null, 2));
             }
-            return {id, ...listaProductos}
+            console.log('lista actualizada')
+            return { id, ...listaProductos }
+
         } catch (error) {
             console.log('Error en actualización')
         }
-
     }
+
+    //El metodo getbyId funciona ok
 
     async getById(id) {
-        const productoEncontrado = obj.find(element => element == id)
-        if (productoEncontrado) {
-            console.log('Producto encontrado: ', productoEncontrado)
-        } else {
-            console.log('El producto buscado no se encontró...')
-        }
-    }
-    async deletById(indeseado) {
-        const productoIndeseado = obj.find(element => element == indeseado)
-        if (productoIndeseado) {
-            console.log('Producto Indeseado: ', productoIndeseado)
-            obj.splice(indeseado, 1)
-            console.log('Nuevo array', obj)
 
-        } else {
-            console.log('El producto que se desea eliminar no se encontró...')
+        console.log('buscar producto por id')
+
+        try {
+            const listaProductos = await this.leer();
+            const indexProduct = listaProductos.findIndex((o) => o.id == id);
+
+            if (indexProduct == -1) {
+                console.log('producto no encontrado')
+                return 'Busqueda erronea'
+            } else {
+                const busqueda = listaProductos[indexProduct];
+                console.log('El producto buscado es', busqueda)
+            }
+            return 'La busqueda terminó';
+
+        } catch (error) {
+            console.log('Error en busqueda')
         }
     }
+
+
+    // identifico el elemento pero no puedo ejecutar desde las lineas 155 a 157
+    async deletById(id) {
+
+        console.log('eliminar archivo por id')
+
+        try {
+            const listaProductos = await this.leer();
+            const indexIndeseado = await listaProductos.findIndex((element) => element.id == id)
+
+            if (indexIndeseado == -1 ) {
+                console.log('El producto indeseado que busca no está en la lista')
+                return 'EL ELEMENTO QUE DESEA ELIMINAR NO SE ENCONTRÓ'
+
+            } else {
+                const busqueda = listaProductos[indexIndeseado];
+                console.log('El producto indeseado es: ', busqueda)
+                listaProductos.splice(indexIndeseado, 1) //borra elemento de la lista
+                await fs.promises.writeFile(this.ruta, JSONS.stringify(listaProductos, null, 2)) //guarda elemento en la lista
+                await this.leer();
+
+            }
+        } catch (error) {
+            return 'no se pudo eliminar nada'
+        }
+
+
+    }
+
     async deleteAll() {
-        obj.splice(0, obj.length)
-        console.log('Array vacío: ', obj)
+
+        console.log ('Se ejecuta la función borrar todo')
+
+        try {
+            const listaProductos = await this.leer();
+            listaProductos.splice(1, 5) // borro los elementos desde la posición 1 hasta la ultima posición del array - Aunque Hardcodee las posiciones con un splice (1,5) la función no se ejecuta
+            await fs.promises.writeFile(this.ruta, JSONS.stringify(listaProductos, null, 2)) //guardo la nueva lista en el archivo
+            await this.leer();
+        } catch (error) {
+            return 'no se pudo eliminar nada'
+        }
     }
 }
 
+async function main() {
 
-
-async function main () {
-
-const usuario = new Contenedor(ruta);
-
-usuario.leer();
-usuario.save(product5);
-usuario.actalizar(2, product2);
-//usuario.getById(id);
-//usuario.deletById(indeseado);
-//usuario.deleteAll();
-
+    const usuario = new Contenedor(ruta);
+    await usuario.leer();
+    //await usuario.save(product3);
+    //await usuario.save(product1);
+    //await usuario.save(product5);
+    //await usuario.save(product3);
+    await usuario.leer();
+    //await usuario.leer();
+    //await usuario.actalizar(4, product4);
+    //await console.log('producto buscado por id');
+    //await usuario.getById(1);
+    await usuario.deletById(1);
+    await usuario.deleteAll();
 }
 
 main();
